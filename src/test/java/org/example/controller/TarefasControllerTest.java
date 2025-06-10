d69c2ef4ad5f5b5364dcc7aa55ad315c9ec50230
gustavogaudsena <gaudereto.sena@gmail.com>
2025-06-10 19:07:31 -0300

    feat: Teste POST endpoint /tarefas [ETAPA 2 - TAREFA 8]

--- src/test/java/org/example/controller/TarefasControllerTest.java
+++ src/test/java/org/example/controller/TarefasControllerTest.java
@@ -1,12 +1,22 @@
 package org.example.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
 import org.example.Database;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TarefasControllerTest {
     private static Javalin app;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @BeforeEach
     public void limpaDatabase() {
         Database.data.clear();
@@ -16,5 +26,27 @@ class TarefasControllerTest {
     public static void setApp() {
         app = Javalin.create(javalinConfig -> javalinConfig.http.defaultContentType = "application/json; charset=UTF-8");
         new TarefasController().registrarRotas(app);
+    }
+
+    @Test
+    @DisplayName(value = "Post no endpoint /tarefas deve retornar status 201")
+    public void testPostNovaTarefa() {
+
+        JavalinTest.test(app, (_, client) -> {
+            String body = """
+                {
+                  "descricao": "Executando tarefa 8 do AT",
+                  "titulo": "Tarefa 8"
+                }
+            """;
+            var response = client.post("/tarefas", body);
+
+            assertEquals(201, response.code(), "Código deve ser 201");
+
+            assert response.body() != null;
+            JsonNode json = objectMapper.readTree(response.body().string());
+            assertTrue(json.has("id"), "Resposta deve contar campo ID");
+            assertTrue(json.has("status"), "Resposta deve contar campo Status");
+        });
     }
 }
