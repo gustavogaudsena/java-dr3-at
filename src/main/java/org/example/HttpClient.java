d552791bc044e6a32f40f57412073e727ac09d29
gustavogaudsena <gaudereto.sena@gmail.com>
2025-06-16 09:32:33 -0300

    feat: HttpClient GET endpoint com pathparams /tarefas/{id} [ETAPA 3 - TAREFA 13]
    Adicionado print do status no GET

--- src/main/java/org/example/HttpClient.java
+++ src/main/java/org/example/HttpClient.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,16 +14,21 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class HttpClient {
+    private static final String SERVER_URL = "http://localhost:7001";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) throws IOException, URISyntaxException {
         try {
-            createTarefa();
-            getTarefas();
+            String newTarefa = createTarefa();
+            JsonNode tarefaJson = objectMapper.readTree(newTarefa);
+            String id = tarefaJson.get("id").asText();
+            getTarefaById(id);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static final String SERVER_URL = "http://localhost:7001" ;
 
     public static String get(String route) throws IOException, URISyntaxException {
         URL url = new URI(SERVER_URL + route).toURL();
@@ -34,6 +42,8 @@ public class HttpClient {
         InputStreamReader streamReader;
         streamReader = new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8);
         BufferedReader in = new BufferedReader(streamReader);
+        int status = conn.getResponseCode();
+        System.out.println("Status: " + status);
 
         String inputLine;
         StringBuilder content = new StringBuilder();
@@ -82,23 +92,30 @@ public class HttpClient {
         return content.toString();
     }
 
-    public static void createTarefa() throws IOException, URISyntaxException {
+    public static String createTarefa() throws IOException, URISyntaxException {
         String mockTitulo = "Nova tarefa da etapa 3 ";
         String mockDescricao = "Criando uma tarefa pelo método Post";
         String data = String.format("""
-                        {
-                          "descricao": "%s",
-                          "titulo": "%s"
-                        }
-                    """, mockDescricao, mockTitulo);
+                    {
+                      "descricao": "%s",
+                      "titulo": "%s"
+                    }
+                """, mockDescricao, mockTitulo);
         String response = post("/tarefas", data);
+
+        System.out.println(response);
+
+        return response;
+    }
+
+    public static void getTarefas() throws IOException, URISyntaxException {
+        String response = get("/tarefas");
 
         System.out.println(response);
     }
 
-
-    public static void getTarefas() throws IOException, URISyntaxException {
-        String response = get("/tarefas");
+    public static void getTarefaById(String id) throws IOException, URISyntaxException {
+        String response = get("/tarefas/" + id);
 
         System.out.println(response);
     }
